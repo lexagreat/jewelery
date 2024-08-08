a57650c74795807ecc396b3e3b67772d46b81af8
lexagreat <9608798010@mail.ru>
2024-08-08 12:33:49 +0300

    init

---
+++ dist/js/app.js
@@ -0,0 +1,32 @@
+(function isWebP() {
+   function testWebP(callback) {
+      let webP = new Image();
+      webP.onload = webP.onerror = function () {
+         callback(webP.height == 2);
+      };
+      webP.src =
+         "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+   }
+   testWebP(function (support) {
+      if (support == true) {
+         document.querySelector("html").classList.add("webp");
+      } else {
+         document.querySelector("html").classList.add("no-webp");
+      }
+   });
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+   burgerWork();
+});
+const body = document.body;
+
+function burgerWork() {
+   const burger = document.querySelector(".header__burger");
+   const menu = document.querySelector(".header__menu");
+   burger.addEventListener("click", () => {
+      burger.classList.toggle("active");
+      menu.classList.toggle("active");
+      body.classList.toggle("lock");
+   });
+}
